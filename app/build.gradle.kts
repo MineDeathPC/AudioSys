97ef426d7a77792436af516e054e7218182b7e4f
MineDeathPC <minedeathpc@gmail.com>
2024-12-21 00:35:50 +0800

    test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(files("libs\\TarsosDSP-2.4.jar"))
+    implementation(libs.androidx.media3.common.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
